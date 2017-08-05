382bae1815c54a3436358b0cd528aa5ea9378c77
theoxo <theoxolausson@gmail.com>
2017-08-05 15:55:08 +0100

    Added further comments

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
   goButton.addEventListener('click', function() {
 
+    // split input into seperate words
     var searchStrings = document.getElementById('input').value.split(/([_\W])/);
 
     // all wrapped in query as it's asynchronous and was acting up :(
@@ -21,23 +22,26 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
   
-
+      // consider only the base website (so results from different subdomains don't come up)
       if (currentUrl.indexOf('/') > -1){
         currentUrl = currentUrl.substr(0, currentUrl.indexOf('/'));
       }
 
+      // characters to remove from the search query
       var badChars="!.,;: ";
 
       var newUrl = "https://www.google.com/#q=";
 
       var searchLength = searchStrings.length;
   
+      // add search words to query
       for (var i = 0; i < searchLength; i++){
         if(badChars.indexOf(searchStrings[i]) == -1){
           newUrl = newUrl + searchStrings[i] + "+";
         }
       }
 
+      // finish off query
       newUrl = newUrl + "-site:" + currentUrl;
     
       openNewTab(newUrl);
